95a1501bb600db218100bb9d590b5bc768624cba
Adrien Faure <adrien.faure@protonmail.com>
2023-12-24 14:06:40 +0100

    day-24: regex

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -1,18 +1,33 @@
-use std::io::BufRead;
 use itertools::Itertools;
 use regex::Regex;
+use std::io::BufRead;
 use std::{fs::File, io::BufReader};
 
-
 fn main() -> std::io::Result<()> {
-    let r_hail = Regex::new(r"(\d+), (\d+), (\d+) @ (\d+), (\d+), (\d+)").unwrap();
+    let r_hail =
+        Regex::new(r"(\d+),[ ]+(\d+),[ ]+(\d+)[ ]+@[ ]+([-]?\d+),[ ]+([-]?\d+),[ ]+([-]?\d+)")
+            .unwrap();
     /* part 1 */
-    let _grid = BufReader::new(File::open("input")?)
+    let hails = BufReader::new(File::open("input")?)
         .lines()
         .filter_map(|line| line.ok())
         .map(|line| {
-            r_hail.captures_iter(line).next().unwrap().extract::<6>().1.into_iter().collect_tuples().unwrap();
-        });
+            println!("{:?}", line);
+            let (x, y, z, vx, vy, vz) = r_hail
+                .captures_iter(&line)
+                .next()
+                .unwrap()
+                .extract::<6>()
+                .1
+                .into_iter()
+                .filter_map(|n| n.parse::<i32>().ok())
+                .collect_tuple()
+                .unwrap();
 
+            ((x, y, z), (vx, vy, vz))
+        })
+        .collect_vec();
+
+    println!("hails: {:?}", hails);
     Ok(())
 }
