19e71e7e8d6550877a57dc993a5782e3938954e0
Adrien Faure <adrien.faure@protonmail.com>
2024-05-26 19:17:25 +0200

    day-12: yeah

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,298 +1,162 @@
 use itertools::Itertools;
 use rayon::prelude::*;
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::io::BufRead;
 use std::iter;
 use std::{fs::File, io::BufReader};
 
-fn check(hotsprings: &Vec<char>, nb_failures: &Vec<u32>) -> bool {
-    // println!("try {:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    // yuck
-
-    let mut total_checked = 0;
-    for (i, (_, group)) in hotsprings
-        .iter()
-        .group_by(|c| **c == '#')
-        .into_iter()
-        .filter(|(match_value, _)| *match_value)
-        .enumerate()
-    {
-        total_checked += 1;
-        if i >= nb_failures.len() {
-            return false;
-        } else {
-            let failure_group: Vec<_> = group.collect();
-            if failure_group.len() as u32 != nb_failures[i] {
-                return false;
-            }
-        }
-    }
-
-    // println!("{:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    // println!("{} {}", total_checked, nb_failures.len());
-    total_checked == nb_failures.len()
-}
-
-fn check_prune(hotsprings: &Vec<char>, nb_failures: &Vec<u32>) -> bool {
-    // println!("try {:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    for (i, (_, group)) in hotsprings
-        .iter()
-        .take_while(|c| **c != '?')
-        .enumerate()
-        .group_by(|(idx, c)| **c == '#')
-        .into_iter()
-        .filter(|(match_value, _)| *match_value)
-        .enumerate()
-    {
-        if i >= nb_failures.len() {
-            return false;
-        } else {
-            let nb_failure_expected = nb_failures[i];
-
-            let failure_group: Vec<_> = group.collect();
-
-            if failure_group.is_empty() || (failure_group.len() as u32) > nb_failure_expected {
-                // println!("fail at: nv_expected_failure: {nb_failure_expected} group_found: {failure_group:?} of len: {}", failure_group.len());
-                return false;
-            }
-        }
-    }
-
-    true
-    // println!("{:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    // println!("{} {}", total_checked, nb_failures.len());
-    // total_checked < nb_failures.len() as u32 && exist
-}
-
-fn recurse(hotsprings: Vec<char>, nb_failures: &Vec<u32>) -> u32 {
-    // println!("{:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    match hotsprings
-        .iter()
-        .enumerate()
-        .filter(|(_, c)| **c == '?')
-        .next()
-    {
-        Some((next_joker_pos, _)) => {
-            let mut add_failure = hotsprings.clone();
-            let mut add_hotsprint = hotsprings.clone();
-
-            add_failure[next_joker_pos] = '#';
-            add_hotsprint[next_joker_pos] = '.';
-
-            let d = if check_prune(&add_failure, &nb_failures) {
-                // print!("f_pass: ");
-                // println!("{:?} {:?}", add_failure.iter().join(""), nb_failures);
-                recurse(add_failure, nb_failures)
-            } else {
-                // print!("f_don't pass: ");
-                // println!("{:?} {:?}", add_failure.iter().join(""), nb_failures);
-                0
-            };
-
-            let f = if check_prune(&add_hotsprint, &nb_failures) {
-                // print!("hot_pass: ");
-                // println!("{:?} {:?}", add_hotsprint.iter().join(""), nb_failures);
-                recurse(add_hotsprint, nb_failures)
-            } else {
-                // print!("hot_don't pass: ");
-                // println!("{:?} {:?}", add_hotsprint.iter().join(""), nb_failures);
-                0
-            };
-
-            f + d
-        }
-        None => {
-            if check(&hotsprings, &nb_failures) {
-                // println!("p1 yeah: {:?}", hotsprings.iter().join(""));
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-}
-
-fn check_incomplete(hotsprings: &Vec<char>, nb_failures: &Vec<u32>) -> (bool, Vec<u32>) {
-    //println!("try {:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    let mut total_checked = Vec::new();
-    let mut exist = false;
-
-    for (i, (_, group)) in hotsprings
-        .iter()
-        .enumerate()
-        .group_by(|(idx, c)| **c == '#')
-        .into_iter()
-        .filter(|(match_value, _)| *match_value)
-        .enumerate()
-    {
-        exist = true;
-        if i >= nb_failures.len() {
-            return (false, Vec::new());
-        } else {
-            let group_size = nb_failures[i];
-            let failure_group: Vec<_> = group.collect();
-
-            let last_char = failure_group.iter().last().unwrap();
-            let first_char = failure_group.iter().next().unwrap();
-
-            if last_char.0 + 1 == hotsprings.len() && last_char.1 == &'#' {
-                total_checked.push(group_size);
-                break;
-            }
-
-            if failure_group.is_empty() || failure_group.len() as u32 != group_size {
-                if first_char.1 == &'#' && first_char.0 == 0 {
-                } else {
-                    return (false, Vec::new());
-                }
-            } else {
-                total_checked.push(group_size);
-            }
-        }
-    }
-
-    if !exist {
-        return (false, [].to_vec());
-    } else {
-        return (true, total_checked);
-    }
-
-    // println!("{:?} {:?}", hotsprings.iter().join(""), nb_failures);
-    // println!("{} {}", total_checked, nb_failures.len());
-    // total_checked < nb_failures.len()
-}
-
-fn chunk_recurse_p2(
-    memo: &mut HashMap<(Vec<char>, Vec<u32>), HashSet<(u32, Vec<u32>)>>,
+fn recurse(
+    consecutive_errors_before: u64,
     hotsprings: Vec<char>,
-    nb_failures: &Vec<u32>,
-) -> Vec<(u32, Vec<u32>)> {
-    if memo.contains_key(&(hotsprings.clone(), nb_failures.to_vec())) {
-        // println!(
-        //     "I know: {} {:?}",
-        //     hotsprings.iter().join(""),
-        //     memo.get(&(hotsprings.clone(), nb_failures.to_vec()))
-        //         .unwrap()
-        // );
-        return memo
-            .get(&(hotsprings, nb_failures.to_vec()))
-            .unwrap()
-            .clone()
-            .into_iter()
-            .collect_vec();
-    }
+    rem_failures: Vec<u64>,
+    // for debug
+    before: Vec<char>,
+    memo: &mut HashMap<(Vec<char>, Vec<u64>, u64), u64>,
+) -> u64 {
+    let str = hotsprings.iter().join("");
+    let before_str = before.iter().join("");
 
-    let res = match hotsprings
-        .iter()
-        .enumerate()
-        .filter(|(_, c)| **c == '?')
-        .next()
-    {
-        Some((next_joker_pos, _)) => {
-            let mut add_failure = hotsprings.clone();
-            let mut add_hotsprint = hotsprings.clone();
-
-            add_failure[next_joker_pos] = '#';
-            add_hotsprint[next_joker_pos] = '.';
-
-            let mut result = Vec::new();
-
-            let mut diese = chunk_recurse_p2(memo, add_failure.clone(), &nb_failures);
-            let mut dot = chunk_recurse_p2(memo, add_hotsprint.clone(), &nb_failures);
-
-            result.append(&mut diese);
-            result.append(&mut dot);
-
-            result
+    match memo.get(&(
+        hotsprings.clone(),
+        rem_failures.clone(),
+        consecutive_errors_before,
+    )) {
+        Some(value) => {
+            // println!(
+            //     "cache hit match {:?} -> {value:?} -- {before_str}|{str}",
+            //     (
+            //         hotsprings.clone().iter().join(""),
+            //         rem_failures.clone(),
+            //         consecutive_errors_before
+            //     )
+            // );
+            return *value;
         }
-        None => {
-            let (checked, nb_match) = check_incomplete(&hotsprings, &nb_failures);
-            if checked {
-                //  println!(
-                //      "yeah: {:?} {:?}\n{:?}",
-                //      hotsprings.iter().join(""),
-                //      nb_failures,
-                //      nb_match
-                //  );
-                [(1, nb_match)].to_vec()
-            } else {
-                [].to_vec()
-            }
-        }
+        None => {}
     };
 
-    memo.insert(
-        (hotsprings, nb_failures.to_vec()),
-        HashSet::from_iter(res.clone()),
-    );
+    // println!("recurse: {consecutive_errors_before} {before_str}|{str} {rem_failures:?}");
+    let mut new_before = before.clone();
 
-    res
-}
-
-fn recurse_p2(hotsprings: Vec<char>, nb_failures: &Vec<u32>, orig_len: u32) -> u32 {
-    // println!("{:?} {:?}", hotsprings.iter().join(""), nb_failures);
-
-    let mut memo = HashMap::new();
-    let mut solutions = HashSet::new();
-
-    for chunk in hotsprings
-        .clone()
-        .into_iter()
-        .chunks(orig_len as usize)
-        .into_iter()
-        .take(1)
-    {
-        let hotspring_chunk = chunk.collect_vec();
-
-        (0..=nb_failures.len()).for_each(|i| {
-            let failures_shift = nb_failures
-                .clone()
-                .into_iter()
-                .cycle()
-                .skip(i)
-                .take(nb_failures.len() + 1)
-                .collect_vec();
-
-            let solution = chunk_recurse_p2(&mut memo, hotspring_chunk.clone(), &failures_shift);
-
-            solutions.extend(solution.into_iter());
-        });
+    let mut idx = 0;
+    let mut new_failures = rem_failures.clone();
+    let mut consecutive_errors = consecutive_errors_before;
+    let mut not_possible = false;
+    'o: for c in &hotsprings {
+        if *c == '#' {
+            consecutive_errors = consecutive_errors + 1;
+        } else if *c == '.' {
+            match new_failures.first() {
+                Some(n) if *n == consecutive_errors => {
+                    // println!("popping {n}");
+                    new_failures = new_failures.into_iter().skip(1).collect_vec();
+                }
+                Some(n) if (*n != consecutive_errors) && consecutive_errors > 0 => {
+                    // println!("number of failure is {n} but stops at {consecutive_errors}");
+                    not_possible = true;
+                }
+                Some(_) => {
+                    // println!("no error yet, anything can happens: {consecutive_errors}");
+                }
+                None => {
+                    if consecutive_errors > 0 {
+                        // println!("consecutive_errors up but {consecutive_errors_before} {new_failures:?}");
+                        not_possible = true;
+                    }
+                    // No more failures to expect
+                    // consecutive_errors = 0;
+                    // break 'o;
+                }
+            }
+            consecutive_errors = 0;
+        } else if *c == '?' {
+            break 'o;
+        }
+        new_before.push(*c);
+        idx = idx + 1;
     }
 
-    let possibilities = memo
-        .iter()
-        .filter(|(_k, v)| !v.is_empty())
-        .filter(|(k, _v)| k.0.iter().all(|c| *c != '?'))
-        .map(|(k, v)| (k.0.clone(), v))
-        .map(|(k, _v)| k)
-        .collect_vec();
+    let remaining_hotsprings = hotsprings.clone().into_iter().skip(idx).collect_vec();
 
-    // println!("p: {:?}", possibilities);
+    if not_possible {
+        // println!(
+        //     "caching impossible {:?} -> 0 --- {before_str}|{str}",
+        //     (
+        //         hotsprings.clone().iter().join(""),
+        //         rem_failures.clone(),
+        //         consecutive_errors_before
+        //     )
+        // );
+        memo.insert(
+            (
+                hotsprings.clone(),
+                rem_failures.clone(),
+                consecutive_errors_before,
+            ),
+            0,
+        );
+        return 0;
+    }
 
-    let all_nb_failures = iter::repeat(nb_failures)
-        .take(5)
-        .flatten()
-        .map(|n| *n)
-        .collect_vec();
+    let debug = remaining_hotsprings.iter().join("");
+    let d_b = new_before.iter().join("");
 
-    let res = itertools::repeat_n(possibilities.iter(), 5)
-        .multi_cartesian_product()
-        .filter(|vv| {
-            check(
-                &vv.into_iter()
-                    .map(|v| v.into_iter())
-                    .flatten()
-                    .map(|c| *c)
-                    .collect_vec(),
-                &all_nb_failures,
-            )
-        })
-        .count();
+    // println!("will check all cases: {d_b}|{debug} -> {new_failures:?}");
 
-    println!("{:?} : {}", hotsprings.iter().join(""), res);
+    let result = if idx == hotsprings.len() {
+        if new_failures.is_empty() {
+            // println!("match {d_b}|{debug} -> {new_failures:?}");
+            1
+        } else {
+            0
+        }
+    } else {
+        let mut add_hotsprint = remaining_hotsprings.clone();
+        add_hotsprint[0] = '.';
+        let f = recurse(
+            consecutive_errors,
+            add_hotsprint,
+            new_failures.clone(),
+            new_before.clone(),
+            memo,
+        );
 
-    res as u32
+        // let d = if consecutive_errors >= *first {
+        let mut add_failure = remaining_hotsprings.clone();
+        add_failure[0] = '#';
+        // println!("d calling with {:?}", (consecutive_errors, add_failure.iter().join(""), new_failures.clone(), new_before.clone()));
+        let d = recurse(
+            consecutive_errors,
+            add_failure.clone(),
+            new_failures.clone(),
+            new_before.clone(),
+            memo,
+        );
+        // println!("d called with {:?}", (consecutive_errors, add_failure.iter().join(""), new_failures.clone(), new_before.clone()));
+        // } else {
+        //    0
+        // };
+        // println!("d: {}, f {}", d, f);
+        d + f
+    };
+
+    // println!(
+    //     "caching end of function {:?} -> {result} --  --- {before_str}|{str}",
+    //     (
+    //         hotsprings.clone().iter().join(""),
+    //         rem_failures.clone(),
+    //         consecutive_errors_before
+    //     )
+    // );
+    memo.insert(
+        (
+            hotsprings.clone(),
+            rem_failures.clone(),
+            consecutive_errors_before,
+        ),
+        result,
+    );
+    return result;
 }
 
 fn main() -> std::io::Result<()> {
@@ -303,8 +167,8 @@ fn main() -> std::io::Result<()> {
             .collect();
 
         let rp1 = 5;
-        let solution: u32 = problems
-            .iter()
+        let solution: u64 = problems
+            .par_iter()
             .map(|line| {
                 let (hotspring_str, failures_str) = line.split_once(" ").unwrap();
                 let hotsrpings =
@@ -318,61 +182,34 @@ fn main() -> std::io::Result<()> {
                         .flatten()
                         .map(|c| c)
                         .collect_vec();
+
                 let nb_failures = iter::repeat(failures_str.split(","))
                     .take(rp1)
                     .flatten()
-                    .filter_map(|n| n.parse::<u32>().ok())
+                    .filter_map(|n| n.parse::<u64>().ok())
                     .collect_vec();
 
                 (hotsrpings, nb_failures)
             })
-            .map(|(hotsprings, failures)| (hotsprings.clone(), recurse(hotsprings, &failures)))
-            .inspect(|(h, s)| println!("{:?} -> {}", h.iter().join(""), s))
+            .map(|(hotsprings, failures)| {
+                (
+                    hotsprings.clone(),
+                    recurse(
+                        0,
+                        hotsprings,
+                        failures.clone(),
+                        Vec::new(),
+                        &mut HashMap::new(),
+                    ),
+                )
+            })
+            // .inspect(|(h, s)| println!("{:?} -> {}", h.iter().join(""), s))
             .map(|(_, s)| s)
             .sum();
 
         println!("p1: {solution:?}");
     }
 
-    if false {
-        let repeat = 5;
-
-        let problems: Vec<_> = BufReader::new(File::open("input")?)
-            .lines()
-            .filter_map(|line| line.ok())
-            .collect();
-
-        let solution: u32 = problems
-            .iter()
-            .map(|line| {
-                let (hotspring_str, failures_str) = line.split_once(" ").unwrap();
-                let hotsrpings =
-                    iter::repeat(hotspring_str.chars().chain(iter::repeat('?').take(1)))
-                        .take(repeat - 1)
-                        // Insert a . at the end
-                        .chain(iter::repeat(
-                            hotspring_str.chars().chain(iter::repeat('.').take(1)),
-                        ))
-                        .take(repeat)
-                        .flatten()
-                        .map(|c| c)
-                        .take((1 + hotspring_str.len()) * repeat)
-                        .collect_vec();
-
-                println!("created {}", hotsrpings.iter().join(""));
-
-                let nb_failures = failures_str
-                    .split(",")
-                    .filter_map(|n| n.parse::<u32>().ok())
-                    .collect_vec();
-
-                (hotsrpings, nb_failures, hotspring_str.len())
-            })
-            .map(|(hotsprings, failures, len)| recurse_p2(hotsprings, &failures, 1 + len as u32))
-            .sum();
-
-        println!("p2: {solution:?}");
-    }
     Ok(())
 }
 
